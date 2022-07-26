ecd5bdc64794353391ca96d903cdc69934423238
zjamali <zouhairjamali1998@gmail.com>
2022-07-26 16:08:31 +0100

    fix merge

---
+++ front-end/pages/2FA.tsx
@@ -0,0 +1,20 @@
+import React, { useContext, useState } from 'react'
+import { ChatContext } from '../context/chatContext'
+import loginStyle from '../styles/Chat.module.css'
+import { v4 as uuidv4 } from 'uuid'
+
+export default function Login(props: any) {
+  // const { state, setMainUser } = useContext(ChatContext)
+
+  return (
+    <div className={loginStyle.loginContainner}>
+      <form
+        className={loginStyle.loginForm}
+        action="http://localhost:5000/auth/42"
+      >
+        <img src="/42logo-white.svg" alt="42logo" />
+        <button type="submit">login</button>
+      </form>
+    </div>
+  )
+}
