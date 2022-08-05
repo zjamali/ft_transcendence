e4f048db9f167ac0ae856fafa9f94c4143a9eacc
zjamali <zouhairjamali1998@gmail.com>
2022-08-05 15:16:19 +0100

    set player status (true/false) : done

--- front-end/components/game/GameOver.tsx
+++ front-end/components/game/GameOver.tsx
@@ -3,7 +3,19 @@ import ParticleBackground from "./ParticleBackground";
 import style from "../../styles/GameOver.module.css";
 import { data } from "./HomeGame";
 import { Data } from "../../Library/Data";
+import socket from "../../Library/Socket";
+import { useContext } from "react";
+import { AppContext } from "../../context/AppContext";
 export function GameOver({ curData }: any) {
+	/*
+	 ** set user status ins playing off :
+	 */
+	const { state } = useContext(AppContext);
+
+	state.eventsSocket.emit("GAME_OVER", state.mainUser.id);
+	/*
+	 */
+
 	let myVar = curData.get_Winner() ? (
 		<div className={style.loser}>
 			<h2>You Lose</h2>
@@ -19,7 +31,7 @@ export function GameOver({ curData }: any) {
 			</div>
 		</div>
 	);
-	curData = new Data(1200, 600);
+
 	return (
 		<div className={style.splashScreen}>
 			<h2>Game Over</h2>
