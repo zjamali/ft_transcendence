f408163ec8941f2c820effcc6a19dd3f4b163128
nowl01 <itsabdennacerama@gmail.com>
2022-08-05 15:38:26 +0100

    refactor: game home page

--- front-end/components/game/HomeGame.tsx
+++ front-end/components/game/HomeGame.tsx
@@ -6,6 +6,10 @@ import socket from "../../Library/Socket";
 import { AppContext } from "../../context/AppContext";
 import axios from "axios";
 import Setting from "./Setting";
+import SettingsIcon from '@mui/icons-material/Settings';
+import { Button, ButtonBase } from "@mui/material";
+import SportsEsportsTwoToneIcon from '@mui/icons-material/SportsEsportsTwoTone';
+
 import { Data } from "../../Library/Data";
 
 //NOTE - Initiale data and Information about all Game like (ball, paddle, score, width, height, canvas)
@@ -112,76 +116,96 @@ export function HomeGame() {
 			});
 	};
 	
-	/* {!isSetting && <Setting setSetting={setSetting} data={data}/>}
-	{!isGame ? (
-		<div className={styles.container}>
-		<div className={styles.game}>
-			<img src="/pingpong.png" alt="Ping Pong Game" />
-		</div>
-		<div className={styles.about}>
-			<div>
-			<h1>
-				Ping
-				<img
-				src="/racket.png"
-				alt="racket"
-				width="80px"
-				height="60px"
-				/>
-				Pong
-			</h1>
-			<p>
-				PING PON is a table
-				tennis game where you can enjoy a real match experience.
-				<br />
-				You can enjoy the feeling of an actual table tennis by tossing
-				and serving the ball, and hitting back to a different direction
-				by adjusting the angle of the racket.
-				<br />
-				You can discorver it by yourself &nbsp;
-				<span className={styles.emoji}>😉</span>
-			</p>
-			<div className={styles.btn}>
-				<button className={styles.btnDef} onClick={handleGame}>
-				PLAY
-				</button>
-				<button className={styles.btnObs} onClick={handleSetting}>
-				SETTING
-				</button>
-		{accepGame && (
-			<button
-				className={styles.btnObs}
-				onClick={gameHandleAcceptInvite}
-			>
-				accept invitation
-			</button>
-		)}
-			</div>
-			</div>
-		</div>
-		</div>
-	) : (
-		<Game
-		data={data}
-		currentState={currentState}
-		setCurrentState={setCurrentState}
-		/>
-	)} */
-
+	
 	return (
-		<div style={{ height: 'calc(100vh - 430px', width: '80%', display: 'flex', alignItems: 'center', justifyContent: 'center', backgroundColor: '#212b36', position: 'relative', top: '50px', border: '1px solid #555f6a', borderRadius: '12px'}}>
-			{!isSetting && <Setting setSetting={setSetting} data={data} />}
-			{!isGame ? 
-				(
-				<div className="Home-game" style={{width: '80%', height: '100%', display: 'flex', flexDirection: 'column'}}>
-					<div style={{color: '#919eab', display: 'flex', alignItems: 'center', justifyContent: 'center'}}><h1 style={{fontWeight: '500', fontSize: '70px'}}>Ping Pong Game</h1></div>
-					<div style={{height: '600px'}}>body</div>
-					<div>foot</div>
+		<>
+		 {!isSetting && <Setting setSetting={setSetting} data={data}/>}
+		{!isGame ? (
+			<div className={styles.container}>
+			<div className={styles.game}>
+				<img src="/pingpong.png" alt="Ping Pong Game" />
+			</div>
+			<div className={styles.about}>
+				<div>
+				<h1>
+					Ping
+					<img
+					src="/racket.png"
+					alt="racket"
+					width="80px"
+					height="60px"
+					/>
+					Pong
+				</h1>
+				<p>
+					PING PON is a table
+					tennis game where you can enjoy a real match experience.
+					<br />
+					You can enjoy the feeling of an actual table tennis by tossing
+					and serving the ball, and hitting back to a different direction
+					by adjusting the angle of the racket.
+					<br />
+					You can discorver it by yourself &nbsp;
+					<span className={styles.emoji}>😉</span>
+				</p>
+				<div className={styles.btn}>
+					<button className={styles.btnDef} onClick={handleGame}>
+					PLAY
+					</button>
+					<button className={styles.btnObs} onClick={handleSetting}>
+					SETTING
+					</button>
+			{accepGame && (
+				<button
+					className={styles.btnObs}
+					onClick={gameHandleAcceptInvite}
+				>
+					accept invitation
+				</button>
+			)}
 				</div>
-				)
-				: 
-				(<Game data={data} currentState={currentState} setCurrentState={setCurrentState} />)
-			}
-		</div>
+				</div>
+			</div>
+			</div>
+		) : (
+			<Game
+			data={data}
+			currentState={currentState}
+			setCurrentState={setCurrentState}
+			/>
+		)} 
+		</>
+		// <div style={{ height: 'calc(100vh - 430px', width: '80%', display: 'flex', alignItems: 'center', justifyContent: 'center', backgroundColor: '#212b36', position: 'relative', top: '50px', border: '1px solid #555f6a', borderRadius: '12px'}}>
+		// 	{!isSetting && <Setting setSetting={setSetting} data={data} />}
+		// 	{!isGame ? 
+		// 		(
+		// 		<div className="Home-game" style={{width: '80%', height: '100%', display: 'flex', flexDirection: 'column'}}>
+		// 			<div style={{color: '#919eab', display: 'flex', alignItems: 'center', justifyContent: 'center'}}><h1 style={{fontFamily: 'Deltha, sans-serif', fontWeight: '900', fontSize: '70px', position: 'relative', bottom: '50px'}}>Ping Pong Game</h1></div>
+		// 			<div style={{height: '85%', display: 'flex', flexDirection: 'row'}}>
+		// 				<div style={{width: '50%', height: '100%', display: 'flex', justifyContent: 'center', alignItems: 'center'}}>
+		// 					<div style={{width: '100%', height: '100%'}}><img src="/pingpong.png" alt="Ping Pong Game" style={{ width: '100%', height: '100%', objectFit: 'fill'}}/></div>
+		// 				</div>
+		// 				<div style={{width: '50%', height: '100%', borderLeft: '0.1px solid #919eab', display: 'flex', alignItems: 'center', justifyContent: 'space-evenly', fontSize: '20px', padding: '15px', flexDirection: 'column'}}>
+		// 					<div style={{width: '100%', marginLeft: '50px', color: '#555f6a', lineHeight: '30px', fontFamily: 'Deltha, sans-serif'}}>
+		// 						PING PONG is a table tennis game where you can enjoy a real match experience.
+		// 						<br />
+		// 						You can enjoy the feeling of an actual table tennis by tossing
+		// 						and serving the ball, and hitting back to a different direction
+		// 						by adjusting the angle of the racket.
+		// 						<br />
+		// 						You can discorver it by yourself &nbsp;
+		// 					</div>
+		// 					<div style={{width: '100%', height: '60px', display: 'flex', flexDirection: 'row', justifyContent: 'space-evenly'}}>
+		// 						<Button variant="outlined" color="warning" size="large" onClick={handleGame} startIcon={<SportsEsportsTwoToneIcon/>}>Play now</Button>
+		// 						<Button variant="outlined" color="info" size="large" onClick={handleSetting}startIcon={<SettingsIcon/>}>Settings</Button>
+		// 					</div>
+		// 				</div>
+		// 			</div>
+		// 		</div>
+		// 		)
+		// 		: 
+		// 		(<Game data={data} currentState={currentState} setCurrentState={setCurrentState} />)
+		// 	}
+		// </div>
 	)
 }
