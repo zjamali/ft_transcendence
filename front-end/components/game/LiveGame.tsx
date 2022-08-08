3f6f557b1bc719a498c8b1b586a4d4aac24f5bcb
zjamali <zouhairjamali1998@gmail.com>
2022-08-08 23:18:27 +0100

    live game redesigne : done

--- front-end/components/game/LiveGame.tsx
+++ front-end/components/game/LiveGame.tsx
@@ -1,38 +1,74 @@
-import { useEffect, useRef, useState } from "react";
+import { useContext, useEffect, useRef, useState } from "react";
 import styles from "../../styles/LiveGame.module.css";
 import CurrentGame from "./CurrentGame";
 import socket from "../../Library/Socket";
-
+import { AppContext } from "../../context/AppContext";
+import { useRouter } from "next/router";
+import Link from "next/link";
 
 export function LiveGame() {
-  const [games, setGames] = useState([]);
+	const [games, setGames] = useState([]);
+	const { state, setOnlineGames } = useContext(AppContext);
+  // const router = useRouter();
 
-  useEffect( () => {
-      socket.on("receive_games", (data: any) => {
-      const tmp = JSON.parse(data);
-      if (tmp.hasOwnProperty("games")) {
-        setGames(tmp.games);
-      }
-    });
-    return () => {
-      socket.off("receive_games");
-    };
-  }, [games]);
-  const gameContainer = useRef(null);
-  return (
-    <div  ref={gameContainer}>
-      {games.length !== 0  ? (
-        games.map((game, index) => {
-          return <CurrentGame key={index} game={game}  gameContainer={gameContainer} />;
-        })
-      ) : (
-        <div className={styles.empty}>
-          <h1>CURRENT GAMES EMPTY</h1>
-        </div>
-      )}
-    </div >
-  );
+	useEffect(() => {
+		socket.on("receive_games", (data: any) => {
+			const tmp = JSON.parse(data);
+			if (tmp.hasOwnProperty("games")) {
+				setGames(tmp.games);
+				setOnlineGames(tmp.games);
+			}
+		});
+		return () => {
+			socket.off("receive_games");
+		};
+	}, [games]);
+
+	const gameContainer = useRef(null);
+	return (
+		<div
+			ref={gameContainer}
+			className="home-game-container"
+			style={{
+				flexDirection: "column",
+				justifyContent: "flex-start",
+				alignItems: "center",
+				padding: "10px",
+			}}
+		>
+			{state.onlineGames.length !== 0 ? (
+				state.onlineGames.map((game, index) => {
+					return (
+						<Link
+							href={{
+								pathname: "live/[liveId]",
+								query: {
+									liveId: `${index}`,
+								},
+							}}
+							key={index}
+						>
+							<a style={{width: '100%', display: 'flex', justifyContent: 'center', alignItems: 'center'}}>
+								<CurrentGame
+									index={index}
+									key={index}
+									game={game}
+									gameContainer={gameContainer}
+								/>
+							</a>
+						</Link>
+					);
+				})
+			) : (
+				<div
+					className={styles.empty}
+					style={{ fontFamily: "Deltha, sans-serif" }}
+				>
+					<h1>NOTHING TO WATCH</h1>
+				</div>
+			)}
+		</div>
+	);
 }
 
 export default LiveGame;
-
---
+++ front-end/pages/live/[liveId]/index.tsx
@@ -0,0 +1,48 @@
+import { useRouter } from "next/router";
+import * as React from "react";
+import CurrentGame from "../../../components/game/CurrentGame";
+import { AppContext } from "../../../context/AppContext";
+import { Data, StateGame } from "../../../Library/Data";
+import socket from "../../../Library/Socket";
+import Game from "../../game";
+
+export interface IAppProps {}
+
+export default function LiveIndex(props: IAppProps) {
+	const router = useRouter();
+	const { liveId } = router.query;
+	const { state } = React.useContext(AppContext);
+	let data = null;
+	data = state.onlineGames[Number(liveId)];
+    console.log("game data : ", data);
+	const gameContainer = React.useRef(null);
+	return (
+		<div className="profile-content">
+			<div
+				ref={gameContainer}
+				className="home-game-container"
+				style={{
+					flexDirection: "column",
+					justifyContent: "flex-start",
+					alignItems: "center",
+					padding: "10px",
+				}}
+			>
+				<div
+					style={{ width: "100%", height: "100%" }}
+					ref={gameContainer}
+				>
+                    {!(data.player_1.score === 5 || data.player_2.score === 5)  
+                    ?
+                        <CurrentGame
+                        game={data}
+                        gameContainer={gameContainer}
+						/>
+
+                        : " game over "
+                    }
+				</div>
+			</div>
+		</div>
+	);
+}
